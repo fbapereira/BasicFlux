3fd9895dadb4e4b8b878ee02a0e71a0e2a218e87
F. de Almeida Pereira <f.de.almeida.pereira@avanade.com>
2017-12-20 12:19:27 -0200

    menu

--- js/init.js
+++ js/init.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('.button-collapse').sideNav();
-
   $(".dropdown-button").dropdown();
   $("#menuDesk").dropdown();
 });
@@ -8,28 +7,30 @@ $(document).ready(function () {
 var myApp = angular.module('myApp', ["ngRoute"])
   .config(function ($routeProvider) {
     $routeProvider
-    .when("/servicosdigitais",{
-      templateUrl: "controller/servicosdigitais.html"
-    })
-    .when("/inovacao",{
-      templateUrl: "controller/inovacao.html"
-    })
-    .when("/sistematizacao",{
-      templateUrl: "controller/sistematizacao.html"
-    })
-    .when("/financeiro",{
-      templateUrl: "controller/financeiro.html"
-    })
       .when("/", {
         templateUrl: "controller/main.html"
+      })
+      .when("/servicosdigitais", {
+        templateUrl: "controller/servicosdigitais.html"
+      })
+      .when("/inovacao", {
+        templateUrl: "controller/inovacao.html"
+      })
+      .when("/sistematizacao", {
+        templateUrl: "controller/sistematizacao.html"
+      })
+      .when("/financeiro", {
+        templateUrl: "controller/financeiro.html"
       })
       .when("/portifolio", {
         templateUrl: "controller/portifolio.html"
       })
-      .when("/green", {
-        templateUrl: "green.htm"
+
+      .when("/app", {
+        templateUrl: "controller/app.html"
       })
-      .when("/blue", {
-        templateUrl: "blue.htm"
-      });
+      .when("/contato", {
+        templateUrl: "controller/contato.html"
+      })
+
   });
